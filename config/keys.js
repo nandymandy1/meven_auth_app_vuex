fcee2cd9c15b3276c5599ee98287c60e9578c611
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-04-25 12:01:33 +0530

    Prodution Database added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-    mongoURI: "mongodb://localhost:27017/meven_auth",
-    secret: 'yoursecret'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        mongoURI: "mongodb://nandymandy123:nandymandy123@ds129906.mlab.com:29906/mevn_auth",
+        secret: 'yoursecret'
+    };
+} else {
+    module.exports = {
+        mongoURI: "mongodb://localhost:27017/meven_auth",
+        secret: 'yoursecret'
+    };
+}
